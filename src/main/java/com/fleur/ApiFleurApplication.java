5ed3f3bcdee3a63049808861345b7d8c89048b3e
ASUS <tarek.bouzayani@gmail.com>
2022-06-03 15:11:25 +0100

    Initial commit

---
+++ src/main/java/com/fleur/ApiFleurApplication.java
@@ -0,0 +1,13 @@
+package com.fleur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFleurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFleurApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fleur/ApiFleurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFleurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
